9b740e20e4bf73138e220cf92cef3cdf1a54df2f
HTML <scv9364@naver.com>
2025-10-02 14:36:34 +0900

    20251002 spring

--- test1/src/main/java/com/example/test1/controller/StuController.java
+++ test1/src/main/java/com/example/test1/controller/StuController.java
@@ -1,6 +1,7 @@
 package com.example.test1.controller;
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,61 +14,80 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.test1.dao.StuService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 @Controller
 public class StuController {
-	
+
 	@Autowired
 	StuService stuService;
-	
-	@RequestMapping("/stu-list.do") 
-    public String login(Model model) throws Exception{ 
-		
-        return "/stu-list";
-    }
-	
-	@RequestMapping("/stu-view.do") 
-    public String view(HttpServletRequest request, Model model, @RequestParam HashMap<String, Object> map) throws Exception{ 
+
+	@RequestMapping("/stu-list.do")
+	public String login(Model model) throws Exception {
+
+		return "/stu-list";
+	}
+
+	@RequestMapping("/stu-view.do")
+	public String view(HttpServletRequest request, Model model, @RequestParam HashMap<String, Object> map)
+			throws Exception {
 		request.setAttribute("stuNo", map.get("stuNo"));
-        return "/stu-view";
-    }
-	
+		return "/stu-view";
+	}
+
 	@RequestMapping(value = "/stu-info.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String login(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		System.out.println(map);
 		resultMap = stuService.stuInfo(map);
-		
+
 		return new Gson().toJson(resultMap);
 	}
-	
+
 	@RequestMapping(value = "/stu-list.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String stuList(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap = stuService.getStuList(map);
-		
+
 		return new Gson().toJson(resultMap);
 	}
-	
+
 	@RequestMapping(value = "/stu-delete.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String stuDelete(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap = stuService.removeStudent(map);
-		
+
 		return new Gson().toJson(resultMap);
 	}
-	
+
 	@RequestMapping(value = "/stu-view.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String view(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap = stuService.searchStudent(map);
-		
+
 		return new Gson().toJson(resultMap);
 	}
-	
+
+	@RequestMapping(value = "/stu/deleteList.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String deleteList(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		System.out.println(map);
+
+		String json = map.get("selectItem").toString();
+		ObjectMapper mapper = new ObjectMapper();
+		List<Object> list = mapper.readValue(json, new TypeReference<List<Object>>() {});
+		map.put("list", list);
+		
+		 resultMap = stuService.removeStudentList(map);
+
+		return new Gson().toJson(resultMap);
+	}
+
 }
